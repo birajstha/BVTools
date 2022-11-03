a722dcaa81740be57158b5cd717324e2cf756ecf
Biraj Shrestha <birajstha@gmail.com>
2022-11-02 23:51:51 -0400

    Print statement modified

--- parallelPort/parallelPortTest.py
+++ parallelPort/parallelPortTest.py
@@ -3,7 +3,7 @@ import time
 p = parallel.Parallel()
 p.setData(0xff)
 for trigger_value in range(0,256):
+    print (f"Sending Trigger : {trigger_value}")
     p.setData(trigger_value)
-    print (trigger_value)
     time.sleep(0.1)
 p.setData(0x00)
